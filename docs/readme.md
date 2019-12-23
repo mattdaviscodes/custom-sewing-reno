5e6038b95b3c2a2e5b6aa19399c174cd4245daa1
Matt Davis <mdavis.tles@gmail.com>
2019-12-22 22:00:36 -0800

    Create build script

---
+++ docs/readme.md
@@ -0,0 +1,3 @@
+This directory contains a complete build of the site for Github Pages.
+Aside from the root, GH only allows publishing from a single subdirectory
+called `docs`, so everything's in here.
--- gruntfile.js
+++ gruntfile.js
@@ -2,6 +2,8 @@ module.exports = function (grunt) {
     require("grunt-browserify")(grunt);
     require("grunt-contrib-connect")(grunt);
     require("grunt-contrib-watch")(grunt);
+    require("grunt-contrib-copy")(grunt);
+    require("grunt-contrib-uglify")(grunt);
 
     grunt.initConfig({
         pkg: grunt.file.readJSON("package.json"),
@@ -14,6 +16,14 @@ module.exports = function (grunt) {
                     browserifyOptions: {debug: true}
                 }
             },
+            build: {
+                src: ["src/**/*.js"],
+                dest: "docs/bundle.js",
+                options: {
+                    transform: [["babelify", {"presets": ["es2015"]}]],
+                    browserifyOptions: {debug: true}
+                }
+            }
         },
         connect: {
             server: {
@@ -29,9 +39,25 @@ module.exports = function (grunt) {
                 ],
                 tasks: ["browserify:dev"],
             },
+        },
+        copy: {
+            build: {
+                files: [
+                    {expand: false, src: ["index.html", "style.css"], dest: "docs/"},
+                    {expand: false, src: ["img/*"], dest: "docs/"}
+                ]
+            }
+        },
+        uglify: {
+            build: {
+                files: {
+                    'docs/bundle.js': ['docs/bundle.js']
+                }
+            }
         }
     });
 
     grunt.registerTask("default", ["dev"]);
     grunt.registerTask("dev", ["browserify:dev", "connect", "watch"]);
+    grunt.registerTask("build", ["copy:build", "browserify:build", "uglify:build"])
 };
